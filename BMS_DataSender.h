0b45dad8a3bbafd7cca32be6f6ce8f3fb26c83cf
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-04-07 12:51:08 +0530

    Update BMS_DataSender.h

--- BMS_DataSender.h
+++ BMS_DataSender.h
@@ -3,4 +3,4 @@
 void readBMSDataFromFile(float* Temperature, float* SOC, float* ChargeRate);
 void sendBMSDataToConsole(float* Temperature, float* SOC, float* ChargeRate);
 void printOnConsole(float TempPrint, float SOCPrint, float ChargeRatePrint);
-void BMS_DataSender();
+void bms_DataSender();
