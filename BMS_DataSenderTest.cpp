3569fa5bc8c1b4c288c1cc4315471141b3f9a8c5
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-05-01 16:34:02 +0530

    Update BMS_DataSenderTest.cpp

--- BMS_DataSenderTest.cpp
+++ BMS_DataSenderTest.cpp
@@ -6,14 +6,27 @@
 
 TEST_CASE("Test to verify the data read")
 {
+  float Temperature_loc, SOC_loc, ChargeRate_loc;
   float Temperature[No_of_Readings], SOC[No_of_Readings], ChargeRate[No_of_Readings] = {0};
+  float ExpectedTemperature[No_of_Readings], ExpectedSOC[No_of_Readings], ExpectedChargeRate[No_of_Readings] = {0};
   readBMSDataFromFile(Temperature, SOC, ChargeRate);
-  float expectedOutput[3][3] = {{42,57,0.68}, {45,58,0.65},{48,60,0.34}};
- 
-  for(int i = 0; i < 3; i++)
-  {
-    REQUIRE(Temperature[i] == expectedOutput[i][0]);
-    REQUIRE(SOC[i] == expectedOutput[i][1]);
-    REQUIRE(ChargeRate[i] == expectedOutput[i][2]);
-  }
+    
+  FILE *file = fopen("./BMS_DataFromConsole.txt","r");                                                                  
+  for(int i=0; i<No_of_Readings; i++)
+   {
+     if (file!=NULL) 
+     {
+        for(int i=0; fscanf(file, "%f,%f\n", &Temperature_loc,&SOC_loc,&ChargeRate_loc)!=EOF; i++)
+        {
+            ExpectedTemperature[i] = Temperature_loc;
+            ExpectedSOC[i] = SOC_loc;
+			ExpectedChargeRate = ChargeRate_loc;
+        }
+     }
+       
+    REQUIRE(Temperature[i] == ExpectedTemperature[i]);
+    REQUIRE(SOC[i] == ExpectedSOC[i]);
+    REQUIRE(ChargeRate[i] == ExpectedChargeRate[i]);
+   }
+   fclose(file);
 }
